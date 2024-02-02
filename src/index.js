c6d8912f55b491785852e1f750a1736ae2f36743
Tshegofatso535 <157506833+Tshegofatso535@users.noreply.github.com>
2024-02-02 22:56:12 +0200

    Added advanced Js
    added functions and real data to make the app live

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class= "weather-app-icon"/>`;
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -51,6 +52,62 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+
+function formatDay(timestamp){
+let date=new Date(timestamp*1000);
+let days =["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+
+return days[date.getDay()];
+
+
+}
+
+function getForecast(city){
+
+    let apiKey = "47073036fbata5f17bdo8b85e82a3ee1";
+     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+     axios(apiUrl).then(displayForecast);
+
+}
+
+function displayForecast(response){
+    let forecastHTML="";
+
+    response.data.daily.forEach(function(day,index){
+
+    if (index<5){
+
+        forecastHTML=
+        forecastHTML+
+        
+        `<div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+       
+       <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+        
+        <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature"> 
+        <strong>${Math.round(day.temperature.maxium)}°</strong>
+         </div>
+        <div class="weather-forecast-temperature">${Math.round(day.temperature.minium)}°</div>
+
+        </div>
+
+        </div>
+    `;
+}
+
+
+
+});
+
+
+let forecastElement=document.querySelector("#forecast");
+forecastElement.innerHTML=forecastHTML;
+}
+
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
